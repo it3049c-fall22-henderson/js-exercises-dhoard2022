3c8efb265ac91a121626fa125e6a92d3b87fe6b9
Dee Hoard <81544817+dhoard2022@users.noreply.github.com>
2022-11-23 18:37:59 -0500

    arrays, objects, log

--- src/4.objects.js
+++ src/4.objects.js
@@ -9,7 +9,11 @@
  *      * must use the `class_of` property in the introduction
  */
 const personObject = {
-
+  first_name: `Darrion`,
+  last_name: `Hoard`,
+  class_of: 2024,
+  full_name: function () {return this.first_name + ` ` + this.last_name;},
+  introduction: function () {return `Welcome ` + this.full_name() + this.class_of;}
 };
 
 module.exports = {
